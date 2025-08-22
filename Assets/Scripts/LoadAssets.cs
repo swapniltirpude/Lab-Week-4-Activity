ba73676a316d7a1f8518f0035a074d6d991c948f
s w a p <swapniltirpude33@gmail.com>
2025-08-22 17:23:40 +1000

    70% progress

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+
+    [SerializeField]
+    private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 postionRed = new Vector3(2.2f, 0, 0);
+        Vector3 postionBlue = new Vector3(-2.2f, 0, 0);
+        Quaternion rotation = Quaternion.identity;
+        GameObject instanceRed = Instantiate(redObj, postionRed, rotation);
+        GameObject instanceBlue = Instantiate(blueObj, postionBlue, rotation);
     }
 
     // Update is called once per frame

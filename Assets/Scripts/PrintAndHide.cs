f19a30b71c19d70b1714fa2547ddaaf0bc4a7a69
s w a p <swapniltirpude33@gmail.com>
2025-08-24 18:11:10 +1000

    100% progress

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -7,9 +7,19 @@ public class PrintAndHide : MonoBehaviour
 
     public Renderer rend;
     private int i = 3;// Start is called before the first frame update
+    private int blueTrigger;
     void Start()
     {
-        
+        if (rend == null)
+        {
+            rend = GetComponent<Renderer>();
+        }
+
+        if (CompareTag("Blue"))
+        {
+            blueTrigger = Random.Range(150, 251);
+            Debug.Log(gameObject.name + " is Blue. Will disable Renderer at i = " + blueTrigger);
+        }
     }
 
     // Update is called once per frame
@@ -18,6 +28,20 @@ public class PrintAndHide : MonoBehaviour
 
         i++;
         Debug.Log(gameObject.name + ":" + i);
-        
+
+        if (CompareTag("Red") && i == 100)
+        {
+            Debug.Log(gameObject.name + " is Red and reached i = 100. Deactivating.");
+            gameObject.SetActive(false);
+        }
+
+        if (CompareTag("Blue") && i == blueTrigger)
+        {
+            if (rend != null)
+            {
+                Debug.Log(gameObject.name + " is Blue and reached i = " + blueTrigger + ". Disabling Renderer.");
+                rend.enabled = false;
+            }
+        }   
     }
 }
